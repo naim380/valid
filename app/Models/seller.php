daa3b1e9b4642146d34bd06ef1fe3f1e2d6d89fb
naim380 <naim.ebrahim@le-campus-numerique.fr>
2021-03-03 21:05:37 +0100

    seller

--- app/Models/seller.php
+++ app/Models/seller.php
@@ -12,13 +12,11 @@ class seller extends Model
     public $timestamps = false;
     protected $guarded = [];
 
-    public function cars()
-    {
-<<<<<<< HEAD
+    public function cars() {
         return $this->hasMany(Car::class); //pour chaque vendeur il y a plusieurs voiture
-=======
-        return $this->hasMany(Car::class) ; //pour chaque vendeur il y a plusieurs voiture
->>>>>>> 4fdc44b684fe96799f8e9349a674cca95193b8fc
     }
+
+
+
 
 }
